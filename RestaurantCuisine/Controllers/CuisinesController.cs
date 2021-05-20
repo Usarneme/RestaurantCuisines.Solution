55c87a8f37be310f2cd98025928f9b5cfbab074e
Usarneme <jamestcoop@gmail.com>
2021-05-20 11:18:49 -0700

    adds route handlers for home and some cuisine pages; styling.
    Co-Authored-By: Marney Mallory <marney.mallory@gmail.com>

---
+++ RestaurantCuisine/Controllers/CuisinesController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using RestaurantCuisine.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantCuisine.Controllers
+{
+  public class CuisinesController : Controller
+  {
+    private readonly RestaurantCuisineContext _db;
+    public CuisinesController(RestaurantCuisineContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/cuisines")]
+    public ActionResult Index()
+    {
+      List<Cuisine> model = _db.Cuisines.ToList();
+      return View(model);
+    }
+
+    [HttpGet("/cuisines/create")]
+    public ActionResult Create()
+    {
+      return View();
+    }
+  }
+}
