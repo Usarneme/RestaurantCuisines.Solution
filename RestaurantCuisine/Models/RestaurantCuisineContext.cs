4dbffefc3bd5687f682685a869637c052416aabf
Usarneme <jamestcoop@gmail.com>
2021-05-20 11:01:53 -0700

    sets up the database context for use by EF
    Co-Authored-By: Marney Mallory <marney.mallory@gmail.com>

--- RestaurantCuisine/Models/RestaurantCuisineContext.cs
+++ RestaurantCuisine/Models/RestaurantCuisineContext.cs
@@ -4,6 +4,14 @@ namespace RestaurantCuisine.Models
 {
   public class RestaurantCuisineContext : DbContext
   {
+    public virtual DbSet<Cuisine> Cuisines { get; set; }
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public RestaurantCuisineContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
 
   }
 }
